a4f884a78b6aad0c98baeca03aca3bb4344c964c
Yiheng <HenryQuan@users.noreply.github.com>
2021-12-06 09:21:31 +1100

    added comments

--- fix-dylib-substrate.sh
+++ fix-dylib-substrate.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+<<'###LICENSE'
+ Based on https://www.jianshu.com/p/8236249edd35
+ Written by HenryQuan
+ Repo: https://github.com/HenryQuan/fix-dylib-substrate
+ 
+ This script will backup the original library and change the path pointing to subtrate.
+ Xcode might be needed for this to work and maybe, it also works on Linux.
+ Use this only for educational or research purposes.
+ 
+ MIT LICENSE
+###LICENSE
+
 usage() {
     echo "Usage: $0 <dylib-path>"
     exit 1
@@ -12,6 +24,7 @@ fi
 DYLIB_PATH="$1"
 COPY_PATH="fixed-$DYLIB_PATH"
 cp "$DYLIB_PATH" "$COPY_PATH"
+# find the line with substrate in it and this is the line we need to change
 SUBSTRATE_LINE=$(otool -L "$COPY_PATH" | egrep substrate)
 
 if [ -z "$SUBSTRATE_LINE" ]; then
@@ -20,7 +33,10 @@ if [ -z "$SUBSTRATE_LINE" ]; then
 fi
 
 echo $SUBSTRATE_LINE
+# split the string by space and get the first element
+# /usr/lib/xxx.dylib (xxx) (xxx) -> /usr/lib/xxx.dylib
 OLD_FRAMEOWRK_PATH=($SUBSTRATE_LINE)
+# update path so that it doesn't crash if the device is not jailbroken
 install_name_tool -change "$OLD_FRAMEOWRK_PATH" @loader_path/libsubstrate.dylib "$COPY_PATH"
 ldid -s "$COPY_PATH"
 echo "Patched and signed $COPY_PATH"
